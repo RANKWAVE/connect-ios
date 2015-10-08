77c196d76ff4647b08d90b9b80560005e84b6ac3
dreamsky <dreamsky@fc97b933-0a9f-4c84-9d40-fd49e6f11896>
2015-10-08 02:07:12 +0000

    Rankcloud SDK Demo
    git-svn-id: svn://infrasys.cloudapp.net/SocialRank-Mobile/trunk/ios/Rankcloud/Connect-Demo@286 fc97b933-0a9f-4c84-9d40-fd49e6f11896

---
+++ RCSDKDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RCSDKDemo
+//
+//  Created by HyoungEun Kim on 2015. 10. 5..
+//  Copyright © 2015년 Rankwave Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
